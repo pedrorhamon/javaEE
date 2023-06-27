0d8a65eafad4b08da37c91d9ec4ed637d77d206f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-06-27 20:01:41 -0300

    criando o metodo de create

--- src/main/java/agenda/controller/Controller.java
+++ src/main/java/agenda/controller/Controller.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import agenda.model.DAO;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
@@ -22,10 +22,16 @@ public class Controller extends HttpServlet {
 		String p_action = request.getServletPath();
 		if (p_action.equals("/main")) {
 			this.contatos(request, response);
+		} else if (p_action.equals("/insert")) {
+			this.novoContato(request, response);
 		}
 		// dao.testeConexao();
 	}
 	
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
 	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
 	}
