98f635f820d14627b4e4d965df280533f1ebe52f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-06-26 22:05:15 -0300

    adicionando validação

--- src/main/webapp/scripts/validator.js
+++ src/main/webapp/scripts/validator.js
@@ -4,5 +4,16 @@
  */
 
 function validar() {
-	alert(testes);
+	let nome = frmContato.nome.value;
+	let fone = frmContato.fone.value;
+	if (nome === "") {
+		alert('Preencha o campo nome');
+		frmContato.nome.focus();
+		return false;
+	} else if (fone === "") {
+		alert('Preencha o campo fone');
+		frmContato.nome.focus();
+		return false;
+	}
+
 }
