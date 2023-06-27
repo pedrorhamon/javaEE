e4d63809db7908330a6535845c003bc6e470fe6d
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-06-27 20:08:30 -0300

    pegando os dados da entidade

--- src/main/java/agenda/controller/Controller.java
+++ src/main/java/agenda/controller/Controller.java
@@ -1,6 +1,7 @@
 package agenda.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,11 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import agenda.model.DAO;
+import agenda.model.JavaBeans;
 
 @WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	private JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -31,7 +34,9 @@ public class Controller extends HttpServlet {
 	}
 	
 	protected void novoContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
 	}
 
 	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
