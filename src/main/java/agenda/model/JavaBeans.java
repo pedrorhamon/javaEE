800854b0bac058fe08fe00a28aa807e2ac6c5a09
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-07-10 21:05:39 -0300

    adicionando a lib correta

--- src/main/java/agenda/model/JavaBeans.java
+++ src/main/java/agenda/model/JavaBeans.java
@@ -1,54 +1,19 @@
 package agenda.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JavaBeans {
 
 	private String idcon;
 	private String nome;
 	private String fone;
 	private String email;
-
-	public JavaBeans() {}
-
-	public JavaBeans(String idcon, String nome, String fone, String email) {
-		this.idcon = idcon;
-		this.nome = nome;
-		this.fone = fone;
-		this.email = email;
-	}
-
-	public String getIdcon() {
-		return idcon;
-	}
-
-	public void setIdcon(String idcon) {
-		this.idcon = idcon;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getFone() {
-		return fone;
-	}
-
-	public void setFone(String fone) {
-		this.fone = fone;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 }
